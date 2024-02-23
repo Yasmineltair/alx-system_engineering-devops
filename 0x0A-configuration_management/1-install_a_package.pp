c31d286426178170810526602c0c785b55f46b7f
Yasmineltair <yasmineltair293@gmail.com>
2024-02-23 04:13:58 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Using Puppet, install flask from pip3.
 
-exec {'puppent-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
