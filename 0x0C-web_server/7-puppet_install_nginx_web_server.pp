a6c3287d72b5f4db69467e976b8968678f4dc36c
Yasmineltair <yasmineltair293@gmail.com>
2024-02-27 13:56:59 -0800

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,22 @@
 # install inginx with puppet
 package { 'nginx':
-  ensure  => 'installed',
+  ensure    => 'present',
 }
 
-file { '/var/www/html/index/html':
-  ensure  => 'file',
-  content => 'Hello World!',
-  mode    => '0644',
-  require => package['nginx'],
+exec { 'install':
+  command   => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider  => shell,
 }
 
-exec { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => package['nginx'],
+exec { 'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www.html/index.html',
+  provider => shell,
+}
+exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec { 'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
