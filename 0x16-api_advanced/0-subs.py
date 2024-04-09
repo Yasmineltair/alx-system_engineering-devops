cb674cb0eb36f830bc1d106cff965bfb50919fd8
Yasmineltair <yasmineltair293@gmail.com>
2024-04-09 13:11:39 +0000

    1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """ doc for method"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Chrome/97.0.4692.71"}
-    response = requests.get(url, allow_redirects=False, headers=headers)
+    response = request.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
