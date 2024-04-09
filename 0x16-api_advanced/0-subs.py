87f6b4cd8a4ec8ece62b5d5203f0843109f691bc
Yasmineltair <yasmineltair293@gmail.com>
2024-04-09 12:46:22 +0000

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" doc for module"""
+
+
+def number_of_subscribers(subreddit):
+    """ doc for method"""
+    url = "https://www.redit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Chrome/97.0.4692.71"}
+    response = request.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
