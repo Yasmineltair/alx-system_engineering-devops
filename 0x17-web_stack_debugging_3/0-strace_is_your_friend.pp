3214334db0413ff22dc70969b62e0ea6b6f96135
Yasmineltair <yasmineltair293@gmail.com>
2024-04-11 14:21:27 +0000

    pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # why Apache is returning a 500 error.
 exec { 'fix wordpress':
-  command       => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-setting.php",
+  command    => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php; sudo service apache2 restart',
+  path       => ['/bin', '/usr/bin', '/usr/sbin']
 }
