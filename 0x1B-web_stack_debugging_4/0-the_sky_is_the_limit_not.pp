945353659c53221b215ebce74b2815a03c132f52
Yasmineltair <yasmineltair293@gmail.com>
2024-04-18 12:30:46 +0000

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
-#install the key
+# install
 exec { 'update ulimit':
-  command   => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 15000\"/' /etc/default/nginx",
-  provider  => 'shell',
+  command  => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 15000\"/' /etc/default/nginx",
+  provider => 'shell',
 }
 
 -> exec { 'restart':
-  command   => 'service nginx restart',
-  provider  => 'shell',
+  command  => 'service nginx restart',
+  provider => 'shell',
 }
