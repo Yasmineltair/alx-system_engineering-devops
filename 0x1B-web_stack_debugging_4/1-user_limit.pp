a5b545ad6ad7c3a865cf5e4ad74f7811c646015f
Yasmineltair <yasmineltair293@gmail.com>
2024-04-18 13:49:38 +0000

    1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Sky is the limit, let's bring that limit higher
 exec { 'update soft limit':
-  command   => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 8192/' /etc/security/limits.conf",
-  provider  => 'shell',
+  command  => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 8192/' /etc/security/limits.conf",
+  provider => 'shell',
 }
 
 exec { 'update hard limit':
-  command   => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 8192/' /etc/security/limits.conf",
-  provider  => 'shell',
+  command  => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 8192/' /etc/security/limits.conf",
+  provider => 'shell',
 }
